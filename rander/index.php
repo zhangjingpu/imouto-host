b2c3551caa3af0294b3c2cf83a474eccfb2043fd
李惟 <liwei@liweideiMac.local>
2015-01-31 10:44:43 +0800

    1.31.4 update@1

--- rander/index.php
+++ rander/index.php
@@ -8,7 +8,7 @@ $path = new Path();
 $hosts = array(
 	array(
 		'path' => 'netsh-ipv4',
-		'desc' => '推荐使用，包含了常用的网站，facebook、twitter待更新，暂不提供，可选择其他源'
+		'desc' => '推荐使用，包含了常用的网站，现已支持facebook、twitter'
 	), 
 	array(
 		'path' => 'netsh-ipv6',
